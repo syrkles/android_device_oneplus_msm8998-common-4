17220a98a65c20cea309e42eaa11e5390be1e174
liquid0624 <rjbirnesser@gmail.com>
2018-03-15 15:45:26 -0400

    msm8998: disable ant radio hal build
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- common.mk
+++ common.mk
@@ -124,10 +124,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
- ANT+
-PRODUCT_PACKAGES += \
-    AntHalService \
-    com.dsi.ant.antradio_library
+#ANT+
+#PRODUCT_PACKAGES += \
+#    AntHalService \
+#    com.dsi.ant.antradio_library
 
 # Bluetooth
 PRODUCT_PACKAGES += \
